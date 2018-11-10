24ef6b6c6aecb4068244741a8a5c27bd0377ae9d
EricRamirezS <eramirezsantis@gmail.com>
2018-11-10 03:31:39 -0300

    removed comments

--- kyu8/Multiply/Multiply.kt
+++ kyu8/Multiply/Multiply.kt
@@ -1,10 +1,3 @@
-/**
- * codewars
- *
- * Created by Eric Ramírez Santis on 08-11-2018.
- * Github Account: https://github.com/EricRamirezS
- *
- */
 fun  multiply(x: Double, y: Double): Double {
     return x * y
 }
