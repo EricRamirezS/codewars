6d5ed529f197d45f1edef803068445e2eec53915
EricRamirezS <eramirezsantis@gmail.com>
2018-11-13 01:48:12 -0300

    fixed "Sudoku Solution Validor" .py file

--- kyu4/SudokuSolutionValidator/Kata.py
+++ kyu4/SudokuSolutionValidator/Kata.py
@@ -1,3 +1,33 @@
-def are_similar(a, b):
-    false=0;
-    for i in range(len()
+def invalidNumbers(numbers):
+    for i in range(1,10):
+        if i not in numbers: return True
+    return False
+
+
+def rowAndColCheck(board):
+    row = []
+    col = []
+    for i in range(9):
+        for j in range(9):
+            row.append(board[i][j])
+            col.append(board[j][i])
+        if invalidNumbers(row) or invalidNumbers(col): return False;
+        row.clear()
+        col.clear()
+    return True
+
+
+def blockCheck(board):
+    block = []
+    for i in range(0,9,3):
+        for j in range(0,9,3):
+            for k in range(3):
+                for l in range(3):
+                    block.append(board[i+k][j+l])
+            if invalidNumbers(block): return False
+            block.clear()
+    return True
+
+
+def done_or_not(board):
+    return 'Finished!' if rowAndColCheck(board) and blockCheck(board) else 'Try again!'
